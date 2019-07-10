c0cb2d2285b7ab4b4e947bd3eab5d7b05086ef4a
hbharatha <46976222+hbharatha@users.noreply.github.com>
2019-07-10 17:42:23 -0500

    added for loop

--- Hello.java
+++ Hello.java
@@ -3,8 +3,9 @@ public class Hello{
  public static void main(String[] args){
 	 
 	for(int i=1;i<=10;i++){
-	  
+	 for(int i=2;i<=12;i++){ 
 		System.out.print("Hello World ..."+i);
+	 }
 		}
 	}
 }
